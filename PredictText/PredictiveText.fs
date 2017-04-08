84cd308fc695e8098a96c3daf8c19bb4562cad4a
mt26691 <tung.nguyen@scopicsoftware.com>
2017-04-08 11:23:49 +0700

    module with dict

---
+++ PredictText/PredictiveText.fs
@@ -0,0 +1,23 @@
+﻿module PredictText.PredictiveText
+
+open System.IO
+open System
+
+let dictPath = Path.Combine(__SOURCE_DIRECTORY__, "dict.txt")
+
+let loadDictFromPath (path:String) = 
+    File.ReadAllLines path
+
+let loadDict (path:String) = 
+    let dict = loadDictFromPath path
+    dict
+
+
+let loadedDict= loadDict dictPath
+
+let autocomplete (prefix:String) (dict:String[]) = 
+    let candidates = dict |> Array.filter(fun word -> word.StartsWith(prefix))
+    candidates
+
+autocomplete "test" loadedDict
+
