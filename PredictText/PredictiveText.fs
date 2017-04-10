01512bff1724ffab1625021c7941a43aa884b353
mt26691 <tung.nguyen@scopicsoftware.com>
2017-04-10 09:28:51 +0700

    predict text

--- PredictText/PredictiveText.fs
+++ PredictText/PredictiveText.fs
@@ -3,21 +3,21 @@
 open System.IO
 open System
 
-let dictPath = Path.Combine(__SOURCE_DIRECTORY__, "dict.txt")
+let private dictPath = Path.Combine(__SOURCE_DIRECTORY__, "dict.txt")
 
-let loadDictFromPath (path:String) = 
+///Allows you to load your own dictionary of data
+let LoadDictFromPath (path:String) = 
     File.ReadAllLines path
 
-let loadDict (path:String) = 
-    let dict = loadDictFromPath path
+///Load dictionary from path
+let LoadDict (path:String) = 
+    let dict = LoadDictFromPath path
     dict
 
-
-let loadedDict= loadDict dictPath
-
-let autocomplete (prefix:String) (dict:String[]) = 
+///Filter data based on prefix and dict
+let Autocomplete (prefix:String) (dict:String[]) = 
     let candidates = dict |> Array.filter(fun word -> word.StartsWith(prefix))
     candidates
 
-autocomplete "test" loadedDict
+
 
